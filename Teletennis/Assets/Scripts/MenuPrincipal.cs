b811dfbd97b27925f9331d358a918f18aed529ba
Eduardo Morato <eduardohenrique110199@gmail.com>
2021-06-06 20:07:45 -0300

    menu principal
    Foi criado um menu principal básico funcional (menos o volume)

---
+++ Teletennis/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jogar (){
+        SceneManager.LoadScene(1);//No botão File > Build settings > Scenes in build dá pra achar o número da cena
+    }
+
+    public void Sair (){
+        Debug.Log ("Teste SAIR");
+        Application.Quit();
+    }
+}
