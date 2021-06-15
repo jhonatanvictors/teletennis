4b7c37f58106b1d8e7c59012b086423789b6f731
Eduardo Morato <eduardohenrique110199@gmail.com>
2021-06-15 00:28:10 -0300

    melhorias
    Algumas coisas foram adicionadas

--- Teletennis/Assets/Scripts/GameManager.cs
+++ Teletennis/Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     public GameObject Player1Prefab;
     public GameObject Player2Prefab;
     public GameObject BolaPrefab;
+    public Text GamerTag1;
+    public Text GamerTag2;
 
     public GameObject GameCanvas;
     public GameObject BotaoComecar;
@@ -15,17 +17,25 @@ public class GameManager : MonoBehaviour
     private void Awake() {
         GameCanvas.SetActive(true);
         BotaoComecar.SetActive(false);
+
+        foreach (PhotonPlayer p in PhotonNetwork.playerList){
+            string nickName = p.NickName;
+            GamerTag1.text = GamerTag1.text + "\n " + nickName;
+        }
     }
+    
 
     public void SpawnPlayer1(){
         PhotonNetwork.Instantiate(Player1Prefab.name, new Vector3(0f,1.20999992f,-12f), Quaternion.identity, 0);
+        
+
     }
 
     public void SpawnPlayer2(){
         PhotonNetwork.Instantiate(Player2Prefab.name, new Vector3(0f,1.20999992f,12f), Quaternion.identity, 0);
     }
 
-    public void SpawnPlayer(){//começar
+    public void SpawnPlayer(){//Spawna a bolinha
         PhotonNetwork.Instantiate(BolaPrefab.name, new Vector3(0f,1.09200001f,0f), Quaternion.identity, 0);
     }
 
--- Teletennis/Assets/Scripts/bola.cs
+++ Teletennis/Assets/Scripts/bola.cs
@@ -7,36 +7,70 @@ public class bola : Photon.MonoBehaviour
 {
     private Vector3 direction;
     public float speed;
+    public float direcao;
 
+    //Referencia os sons
+    public AudioSource sound0;
+    public AudioSource sound1;
     
     //private int playerOneScore;
     
     //private int playerTwoScore;
 
     public Vector3 spawnPoint;
-
     public GameObject goalEffect;
     public GameObject goalEffect2;
 
     //public Text playerOneText;
     //public Text playerTwoText;
 
-    public Renderer rend;
+    //public Renderer rend;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
+        //Sistema de som
+        
+
+        //rend = GetComponent<Renderer>();
+        //rend.enabled = true;
         //playerOneScore = 0;
         //playerTwoScore = 0;
-        
+        direcao = 1f;
         Invoke("comecar",2);
     }
 
+    public void roleta(){
+        if (direcao == 1f){
+            this.direction = new Vector3 (1f,0f,1f);
+            direcao++;
+        } else if (direcao == 2f){
+            this.direction = new Vector3 (1f,0f,-1f);
+            direcao++;
+        } else if (direcao == 3f){
+            this.direction = new Vector3 (-1f,0f,1f);
+            direcao++;
+        } else {
+            this.direction = new Vector3 (-1f,0f,-1f);
+            direcao = 1f;
+        }
+    }
+
+
     public void comecar(){
-        this.direction = new Vector3 (1f,0f,1f);
+        roleta();
+        
+    }
+
+    public void PlaySound0()
+    {
+        sound0.Play();
+    }
+
+    public void PlaySound1()
+    {
+        sound1.Play();
     }
 
     // Update is called once per frame
@@ -56,25 +90,34 @@ public class bola : Photon.MonoBehaviour
     {
         Vector3 normal = col.contacts[0].normal;
         direction = Vector3.Reflect(direction, normal);
+        speed = speed * 1.01f;
 
         if (col.gameObject.name == "paredeSul"){
             //playerTwoScore++;
             Instantiate(goalEffect2, this.transform.position, Quaternion.identity);
             transform.position = spawnPoint;
-            rend.enabled = true;
+            this.direction = new Vector3 (0f,0f,0f);
+            Invoke("comecar",1);
+            PlaySound1();
+
+            //rend.enabled = true;
             
-        } else {
-            Instantiate(goalEffect, this.transform.position, Quaternion.identity);
         }
         
         if (col.gameObject.name == "paredeNorte"){
             //playerOneScore++;
             Instantiate(goalEffect2, this.transform.position, Quaternion.identity);
             transform.position = spawnPoint;
-            rend.enabled = true;
-        } else {
+            this.direction = new Vector3 (0f,0f,0f);
+            Invoke("comecar",1);
+            PlaySound1();
+
+            //rend.enabled = true;
+        } 
+        
+        if (col.gameObject.name != "paredeNorte" && col.gameObject.name != "paredeSul") {
             Instantiate(goalEffect, this.transform.position, Quaternion.identity);
-            
+            PlaySound0();
         }
 
         //if (col.gameObject.name == "jogador1"){
